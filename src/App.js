80c10123bcfd161d373bd643de33ddf7c6ecbce8
Soylatte <kksoyeon61@gmail.com>
2023-08-20 20:59:58 +0200

    creating blog to introduce barcelona

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  let post = 'Things to do in Barcelona';
+  //  post 에 잠깐 보관하는 방식
+  // post를 h4에 삽입
+  
+  return (
+    <div className="App">
+      <div className="black-nav">
+        <h3>Blog</h3>
+      </div>
+      <h4>Food spot</h4>
+      <p>20.08.23</p>
+      
+      </div>
+  );
+}
+
+export default App;
