14eeb35f9ddff0a90be9392dd3012ca11e1640ff
Soylatte <kksoyeon61@gmail.com>
2023-08-28 12:41:47 +0200

    changed mode but not changed

--- src/App.js
+++ src/App.js
@@ -20,7 +20,14 @@ function Nav(props){
   for(let i=0; i<props.topics.length;i++){
     //topics.length 만큼 반복
     let t = props.topics.[i];
-    lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>)
+    lis.push(<li key={t.id}>
+      <a id={t.id} href={'/read/'+t.id} onClick={(event=>{
+        event.preventDefault();
+        props.onChangeMode(event.target.id);
+        // event를 유발시킨 tag 호출
+        // 내부 function 호출
+      })}>{t.title}</a></li>)
+      // parameter 가 한개인 경우 괄호 생략가능
     // link 생성
 
   }
@@ -62,10 +69,14 @@ function App() {
   }
   return (
     <div>
-      <Header title="WEB" onChangeMode={function()
-      alert('Header')}></Header>
-      <Nav topics={topics}></Nav>
-      {content}
+      <Header title="WEB" onChangeMode={function(){
+      mode = 'WELCOME';
+    }}></Header>
+    <Nav topics={topics} onChangeMode={(id)=>{
+       mode = 'READ';
+       // mode 의 값을 바꿔도 변하지 않음 
+      }}></Nav>
+      <Article title="Welcome" body="Hello, WEB"></Article>
     </div>
   );
 
