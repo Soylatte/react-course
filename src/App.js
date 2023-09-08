0ccf8fc67cd07308d7f864f2c753a97aeb7fd3a7
Soylatte <kksoyeon61@gmail.com>
2023-09-08 16:55:13 +0200

    when i have to use State

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
-
+//  when i have to use State?
+// 변수는 갑자기 변경되면 HTML 도 변경해야함 자동으로 반영이 안됨
+// state는 갑자기 변경되면 state쓰던 HTML 은 자동 재렌더링됨
 
 function App() {
   let post ='강남 우동 맛집';
@@ -12,7 +14,7 @@ function App() {
   // let a = num[0] 그럼 a 는 1
   // let c= num[2] c 는 3
   // let [a,c] = [1,2]; 오른쪽 왼쪽 형태를 맞추게 해 a에는 1 c에는 2가 들어가게됨
-  
+
 
 
   return ( // 하나의 태그로 시작해서 끝나야됨
