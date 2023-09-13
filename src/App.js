1f9bbc24a25eabe4baaf0a736ba1a1ec26d0f5ff
Soylatte <kksoyeon61@gmail.com>
2023-09-13 17:15:43 +0200

    onClick in react

--- src/App.js
+++ src/App.js
@@ -7,7 +7,23 @@ import { createRoot } from 'react-dom/client';
 const container = document.getElementById('app');
 const root = createRoot(container);
 // passing a variable to render ()
-    
+const myList = (
+  <ul>
+    <ii></ii>
+    <li></li>
+    <li></li>
+  </ul>
+);
+root.render(myList);    
+mport React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+root.render(<h1>{2 + 3}</h1>);
+// {2 +3} = > 5 출력
+const math = <h1>2 + 3 = 2 + 3</h1>
+root.render(math);
 
 
 
